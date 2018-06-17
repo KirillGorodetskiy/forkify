36e8f4ae6687e6db30304a79141d34c3764488d5
Kirill Gorodetskiy <kirill.gorodetskiy@gmail.com>
2018-06-17 20:03:13 +0500

    Add loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app
  * - Search jblect
@@ -14,7 +14,7 @@ const state = {};
 const controlSearch = async () => {
 	// 1. Get query from the view
 	const query = searchView.getInput(); //TODO
-	console.log(query);
+//	console.log(query);
 	if (query) {
 	// 2. New search object and add to state
 	state.search = new Search(query);
@@ -22,10 +22,12 @@ const controlSearch = async () => {
 	// 3. Prepare UI for the results
 	searchView.clearInput();
 	searchView.clearResults();
+	renderLoader(elements.searchRes);
 	// 4. Search for recipes
 	await state.search.getResults();
 	
 	//5. Render results on UI
+	clearLoader();
 	searchView.renderResults(state.search.results);
 	} 
 	
